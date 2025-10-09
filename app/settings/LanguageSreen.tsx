a5da58f98a6db8113ac746aac89d649acd3ea775
ALBERT AMOAKO <orgin11phile@gmail.com>
2025-10-09 11:24:38 +0000

    fix: Adjust layout styles in LanguageScreen for better alignment and spacing

--- app/settings/LanguageSreen.tsx
+++ app/settings/LanguageSreen.tsx
@@ -166,7 +166,7 @@ const styles = StyleSheet.create({
     marginHorizontal: -6,
   },
   languageCard: {
-    width: '48%',
+    width: '45%',
     backgroundColor: '#FFFFFF',
     borderRadius: 16,
     padding: 16,
@@ -188,11 +188,13 @@ const styles = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'flex-start',
+    alignItems: 'center',
     marginBottom: 12,
   },
   languageFlag: {
     fontSize: 40,
+    textAlign: 'center',
+    flex: 1,
   },
   checkmark: {
     position: 'absolute',
